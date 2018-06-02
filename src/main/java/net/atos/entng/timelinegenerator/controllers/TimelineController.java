ee03c6a2152f67329ce99b5d7134c9f6f0d0368b
dboissin <damien.boissin@web-education.net>
2018-06-02 11:12:19 +0200

    [Evo] global share and notify

--- src/main/java/net/atos/entng/timelinegenerator/controllers/TimelineController.java
+++ src/main/java/net/atos/entng/timelinegenerator/controllers/TimelineController.java
@@ -161,4 +161,31 @@ public class TimelineController extends MongoDbControllerHelper {
 		removeShare(request, false);
 	}
 
+	@Put("/share/resource/:id")
+	@ApiDoc("Share timeline by id.")
+	@SecuredAction(value = "timelinegenerator.manager", type = ActionType.RESOURCE)
+	public void shareResource(final HttpServerRequest request) {
+		UserUtils.getUserInfos(eb, request, new Handler<UserInfos>() {
+			@Override
+			public void handle(final UserInfos user) {
+				if (user != null) {
+					final String id = request.params().get("id");
+					if(id == null || id.trim().isEmpty()) {
+						badRequest(request, "invalid.id");
+						return;
+					}
+
+					JsonObject params = new JsonObject();
+					params.put("profilUri", "/userbook/annuaire#" + user.getUserId() + "#" + user.getType());
+					params.put("username", user.getUsername());
+					params.put("timelineUri", "/timelinegenerator#/view/" + id);
+					params.put("resourceUri", params.getString("timelineUri"));
+
+					shareResource(request, "timelinegenerator.share", false, params, "headline");
+				}
+			}
+		});
+	}
+
+
 }
