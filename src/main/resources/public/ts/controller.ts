583d1d1bdca9e304ec41a5cb8ee1f805b1f14d4b
richard.bunel@opendigitaleducation.com <richard.bunel@opendigitaleducation.com>
2019-10-31 14:57:57 +0100

    fix(bpr): #28006, temporarily disabled videos in title when printing

--- src/main/resources/public/ts/controller.ts
+++ src/main/resources/public/ts/controller.ts
@@ -178,12 +178,12 @@ export const timelineGeneratorController = ng.controller('TimelineGeneratorContr
         $scope.previewMode = true;
         Behaviours.applicationsBehaviours.timelinegenerator.sniplets.timelines.controller.source = timeline;
         timeline.open(async function () {
-            for(const ev of timeline.events.all){
+            /*for(const ev of timeline.events.all){
                 if(ev.video){
                     ev.videoHtml = await embedderService.getHtmlForUrl(ev.video, true);
                     ev.videoHtmlTrusted = $sce.trustAsHtml(ev.videoHtml);
                 }
-            }
+            }*/
             template.close('main');
             template.open('timelines', 'print-timeline');
             setTimeout(()=>window.print(),2500);
